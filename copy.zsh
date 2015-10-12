6aecfbef05ab7ae13bfd9fba01d64dfa92cd28e8
Daisuke Inoue <hibariyashiki@gmail.com>
2015-10-12 15:49:20 +0900

    copy.zsh: wrote comments.

--- copy.zsh
+++ copy.zsh
@@ -3,7 +3,12 @@ stem="$HOME/dat/sleep"
 origin="$stem/origin"
 dest="$stem/dest"
 for i in $origin/*
+# origin の下に tanaka とか yamada といふ dir (擔當者ごと) があり，
+# さらにその下に 1 とか 2 とかといふ dir (何囘めの提出かを示す) がある，
+# といふ想定．
 do
 		leaf=`ls $i -t | head -n 1`
+		# 最新の提出を相手にする．
+		# ls の -t は更新時刻の順に竝べる option.
 		cp $i/$leaf/*.csv $dest
 done
